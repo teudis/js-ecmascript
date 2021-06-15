be36f1b03b736a7899da86cedf8d9752fbc9b8e1
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-06-15 15:48:15 -0400

    ES6 CONCATENATION

---
+++ Concat.js
@@ -0,0 +1,11 @@
+//Concat before
+
+hello = "Hello"
+world = "World"
+cad = hello + " " + world
+console.log(cad)
+
+//Concat ES6
+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);

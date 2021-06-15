e86c0547df821758a0b748e975ccecf58b069dac
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-06-15 16:17:41 -0400

    to USE SPREAD OPERATOR

--- src/es6/desestructuration.js
+++ src/es6/desestructuration.js
@@ -11,3 +11,24 @@ console.log(person.name, person.age)
 
 let {name,country} = person
 console.log(name, country)
+
+
+// SPREAD OPERATOR
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+/*
+console show
+[ 'David',
+'Oscar',
+'Julian',
+'Ricardo',
+'Valeria',
+'Yesica',
+'Camila' ]
+
+*/

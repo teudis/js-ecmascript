d5307ccebdc9e01b3c9c00aeac80ebdf8486226e
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-06-15 15:59:24 -0400

    Multiline

---
+++ multiline.js
@@ -0,0 +1,9 @@
+let sentence = "Example the multiline before ES6 \n"
++ "other line"
+
+// ES6
+let sentence2 = `otra frase epica que necesitamos
+ahora es otra frase epica
+`
+console.log(sentence)
+console.log(sentence2)

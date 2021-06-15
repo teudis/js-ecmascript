9388c6e1cb998e88d6f022eab4cf72548517d502
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-06-15 17:07:27 -0400

    News in ES7, includes and operator ** for pow

---
+++ src/es7/index.js
@@ -0,0 +1,17 @@
+// use method includes
+let numbers = [3,5,2,7,9]
+
+if(numbers.includes(7))
+{
+    console.log("Se encuetra el valor 7")
+}
+else{
+    console.log("No se encuetra el valor 7")
+}
+
+// use operator ** for pow
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
